98da044b8a1ec0350ac4fd00b1e477bd69b4db81
Peter <peter_p@aon.at>
2026-01-05 15:23:33 +0100

    SOCAT optional

--- hacomfoairmqtt/run.sh
+++ hacomfoairmqtt/run.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
-
+# MQTT vom Supervisor
 export MQTT_HOST=$(bashio::services mqtt | jq -r '.host')
 export MQTT_PORT=$(bashio::services mqtt | jq -r '.port')
 export MQTT_USER=$(bashio::services mqtt | jq -r '.username')
 export MQTT_PASS=$(bashio::services mqtt | jq -r '.password')
 
-# ComfoAir TCP target from options
-COMFOAIR_HOST=$(bashio::config 'comfoair_host')
-COMFOAIR_PORT=$(bashio::config 'comfoair_port')
+USE_SOCAT=$(bashio::config 'use_socat')
+SERIAL_DEVICE=$(bashio::config 'Serial_port')
+export SERIAL_DEVICE
 
-bashio::log.info "Starting socat TCP → PTY (/tmp/comfoair)"
-/usr/bin/socat \
-  PTY,link=/tmp/comfoair,raw,echo=0 \
-  TCP:${COMFOAIR_HOST}:${COMFOAIR_PORT} &
+if [ "$USE_SOCAT" = "true" ]; then
+    COMFOAIR_HOST=$(bashio::config 'comfoair_host')
+    COMFOAIR_PORT=$(bashio::config 'comfoair_port')
 
-# give socat time to create PTY
-sleep 1
+    bashio::log.warning "SOCAT enabled → ${COMFOAIR_HOST}:${COMFOAIR_PORT} → ${SERIAL_DEVICE}"
 
-bashio::log.info "Starting haComfoAir MQTT (TCP only)"
-bashio::log.warning "### TCP-ONLY VERSION STARTED ###"
+    /usr/bin/socat \
+      PTY,link=${SERIAL_DEVICE},raw,echo=0 \
+      TCP:${COMFOAIR_HOST}:${COMFOAIR_PORT} &
 
+    sleep 1
+else
+    bashio::log.warning "SOCAT disabled → using serial device ${SERIAL_DEVICE}"
+fi
+
+bashio::log.info "Starting haComfoAir MQTT"
 python3 /app/ca350.py
